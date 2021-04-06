331114d3456147b99fd3f6cc6b1a16f4ece0cb1a
Даниил Чернюк <meggi.mo@yandex.ru>
2021-04-06 18:34:22 +0300

    добавлены методы show index в Project Controller

--- app/Http/Controllers/Project.php
+++ app/Http/Controllers/Project.php
@@ -11,4 +11,9 @@ class Project extends Controller
         $articles = "Hello";
         return view('projects.index',['projects' => $articles]);
     }
+
+    public function show($id)
+    {
+        return view('projects.show',['project' => $id]);
+    }
 }
