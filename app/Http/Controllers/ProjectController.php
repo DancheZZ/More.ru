e5aad5dbcb04a3d9a763d529c6146db17b749b32
Даниил Чернюк <meggi.mo@yandex.ru>
2021-04-14 22:33:03 +0300

    метод listShow завершен

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,14 +7,6 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    /*public function index()
-    {
-        //показывает список проектов, в дальнейшем нужно будет реализовать постраничный вывод
-        $projects = "Hello";
-        $kek = new Project();
-        return view('projects.index',['projects' => $projects]);
-    }*/
-
     //метод для вывода проектов на главной странице постранично ($page) согласно какому-то типу ($type)
     //возможные значения для type: all(все),popular(популярные),new(новые),soon(скоро).
     public function mainShow($type,$page)
@@ -25,34 +17,39 @@ class ProjectController extends Controller
         {
             //выдаются проекты в случайном порядке
             case 'all':
-                $projects = Project::where([
-                    ['published',1],
-                    ['completed',0] ])->inRandomOrder()->skip(3*$page-3)->limit(3)->get();
+                $projects = Project::where
+                ([
+                    ['published',1] 
+                ])->inRandomOrder()->skip(3*$page-3)->limit(3)->get();
                 //skip - пропускает несколько записей, limit - предел вывода
                 //inRandomOrder() - обеспечивает случайный порядок
                 break;  
 
             //сортировка по кол-ву лайков по убыванию
             case 'popular':
-                $projects = Project::where([
-                    ['published',1],
-                    ['completed',0] ])->orderBy('count_likes','desc')->skip(3*$page-3)->limit(3)->get();
+                $projects = Project::where
+                ([
+                    ['published',1]
+                ])
+                    ->orderBy('count_likes','desc')->skip(3*$page-3)->limit(3)->get();
                  //descend - спускаться
                 //ascend - подниматься
                 break;
 
             //сортировка по дате создания, для простоты берется сортировка по id, что эквивалентно и даже лучше, чем по start_date
             case 'new':
-                $projects = Project::where([
-                    ['published',1],
-                    ['completed',0] ])->orderBy('id','desc')->skip(3*$page-3)->limit(3)->get();
+                $projects = Project::where
+                ([
+                    ['published',1]
+                ])->orderBy('id','desc')->skip(3*$page-3)->limit(3)->get();
                 break;
 
-            //сортировка по дате завершения по у
+            //сортировка по дате завершения по final_date по убыванию
             case 'soon':
-                $projects = Project::where([
-                    ['published',1],
-                    ['completed',0] ])->orderBy('finaldate','desc')->skip(3*$page-3)->limit(3)->get();
+                $projects = Project::where
+                ([
+                    ['published',1]
+                ])->orderBy('final_date','desc')->skip(3*$page-3)->limit(3)->get();
                 break;
         }
         return view('main')->with(array('projects'=>$projects));
@@ -61,22 +58,79 @@ class ProjectController extends Controller
     //{type}/{sorting}/{page}
     //метод для вывода всех проектов на странице всех проектов
     //возможные значения type:
-    //working(собирают),completed(завершились),record(рекордсмены).
+    //working(собирают), только незавершенные проекты начиная новыми
+    //completed(завершились), только завершенные проекты 
+    //record(рекордсмены). сортировка по собранным деньгам по убыванию
+    //
+    //возможные значения sorting
+    //popular (сначала с наибольшим количеством лайков)
+    //unknown (сначала с наименьшим количеством лайков)
+    //new (сначала новые)
+    //old (сначала старые)
+
     public function listShow($type,$sorting,$page)
     {
         $projects = null;
+        $forward = null; //по убыванию или возрастанию (desc или ask)
+        $field = null; //поле по которому сортировка (count_likes или start_date)
+        /*if($sorting == 'new' || $sorting=='unknown')
+        {
+            $forward = 'asc';
+        }*/
+
+        switch($sorting)
+        {
+            case 'popular':
+                $forward = 'desc';
+                $field = 'count_likes';
+                break;
+            case 'unknown':
+                $forward = 'asc';
+                $field = 'count_likes';
+                break;
+            case 'new':
+                $forward = 'desc';
+                $field = 'id';
+                break;
+            case 'old':
+                $forward='asc';
+                $field = 'id';
+                break;
+        }
+
+        switch($type)
+        {
+            case 'working':
+                $projects = Project::where
+                ([
+                    ['published',1],
+                    ['completed',0] 
+                ])
+                    ->orderBy($field,$forward)->skip(3*$page-3)->limit(3)->get();
+                break;
+            case 'completed':
+                $projects = Project::where
+                ([
+                    ['published',1],
+                    ['completed',1] 
+                ])
+                    ->orderBy($field,$forward)->skip(3*$page-3)->limit(3)->get();
+                break;
+            case 'record':
+                $projects = Project::where
+                ([
+                    ['published',1]
+                ])
+                ->orderBy('collected_money','desc')->orderBy($field,$forward)->skip(3*$page-3)->limit(3)->get();
+                break;
+        }
+
         
+    
         return view('projects.list', ['projects'=>$projects]);
     }
     
-    /* public function show($id,$type)
-    {
-        //показывает вьюшку конкретного проекта
-        $project = Project::find($id);
-        //будет выведен проект либо с описанием в нижнем подразделе, либо спонсоры, либо комментарии
-        return view('projects.show',['project' => $project,'type'=>$type]);
-    }*/
-
+    
     public function store()
     {
         //сохраняет новый ресурс
@@ -101,4 +155,14 @@ class ProjectController extends Controller
     {
         //ясен пень уничтожение
     }
+
+
+    /* public function show($id,$type)
+    {
+        //показывает вьюшку конкретного проекта
+        $project = Project::find($id);
+        //будет выведен проект либо с описанием в нижнем подразделе, либо спонсоры, либо комментарии
+        return view('projects.show',['project' => $project,'type'=>$type]);
+    }*/
+
 }
--- resources/views/projects/list.blade.php
+++ resources/views/projects/list.blade.php
@@ -1,2 +1,3 @@
-{{ $projects }}
-
+@foreach ($projects as $project)
+    {{ $project->id }}
+ @endforeach
